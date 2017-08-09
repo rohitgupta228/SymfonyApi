5dea5f39b275e72a696ed285674d97e0e9b9c06f
rohit <rohit.gupta@kelltontech.com>
2017-08-09 18:29:58 +0530

    code update

--- src/SymfonyApiBundle/Controller/ApiController.php
+++ src/SymfonyApiBundle/Controller/ApiController.php
@@ -7,6 +7,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
+use Exception;
 
 class ApiController extends DefaultController
 {
@@ -16,11 +17,19 @@ class ApiController extends DefaultController
      */
     public function numberAction()
     {
-        $response = $this->responseGenerator;
-        $number = mt_rand(0, 100);
-        $response->setResponseData($number);
-        $response->setStatus(true);
-        $response->setMessage($this->get('translator')->trans('messages.success'));
+        try
+        {
+            $response = $this->responseGenerator;
+            $number = mt_rand(0, 100);
+            $response->setResponseData($number);
+            $response->setStatus(true);
+            $response->setMessage($this->get('translator')->trans('messages.success'));
+        }
+        catch (Exception $ex)
+        {
+            $message = $ex->getMessage();
+            $response->setMessage($message);
+        }
         $serviceResponse = $response->getServiceResponse();
         return new Response($serviceResponse);
     }
